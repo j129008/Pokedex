6ee35584257110d012323e14c5720685e192e9f6
davidchang <davidchang@ovotv.com>
2021-06-07 09:20:00 +0000

    add add_api

--- basic/api.py
+++ basic/api.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 from basic.models import Info as InfoModel
+from basic.models import Type as TypeModel
+from db import db
 
 basic_api = Blueprint('basic', __name__)
 
@@ -13,3 +16,24 @@ def info(pid):
         return jsonify(info_obj.to_json())
 
     return jsonify({'Status': 'Pokemon not found'}), 404
+
+
+@basic_api.route('/add', methods=['POST'])
+def add_pokemon():
+    req_data = request.json
+    if not InfoModel.verify(req_data):
+        return jsonify({'Status': 'Invalid Input'}), 406
+
+    info_obj = InfoModel()
+    info_obj.from_json(req_data)
+    db.session.add(info_obj)
+    db.session.flush()
+
+    for t in req_data['types']:
+        type_obj = TypeModel()
+        type_obj.from_json({'pid': info_obj.id, 'type': t})
+        db.session.add(type_obj)
+
+    db.session.commit()
+
+    return jsonify({'Status': 'Create Success'})
