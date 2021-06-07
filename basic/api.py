92eb86796e8eb0b9bd9ace98f62efdb6dbcef278
davidchang <davidchang@ovotv.com>
2021-06-07 02:36:15 +0000

    info api return db data

--- basic/api.py
+++ basic/api.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
 from flask import jsonify
+from basic.models import Info as InfoModel
 
 basic_api = Blueprint('basic', __name__)
 
 
-@basic_api.route('/info', methods=['GET'])
-def info():
-    return jsonify({'Status': 'hello world'})
+@basic_api.route('/info/<int:pid>', methods=['GET'])
+def info(pid):
+    info_obj = InfoModel.query.filter_by(id=pid).first()
+
+    if info_obj:
+        return jsonify(info_obj.to_json())
+
+    return jsonify({'Status': 'Pokemon not found'}), 404
