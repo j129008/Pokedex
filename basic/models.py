4fa302ea2c1cd01fb86af91dd8668f3062286b0d
davidchang <davidchang@ovotv.com>
2021-06-07 03:02:41 +0000

    add Evolution model

--- basic/models.py
+++ basic/models.py
@@ -8,6 +8,23 @@ class PokemonType(enum.Enum):
     Fire = 3
 
 
+class Evolution(db.Model):
+    __table_args__ = {'mysql_charset': 'utf8mb4'}
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    before = db.Column(db.BigInteger)
+    after = db.Column(db.BigInteger)
+
+    def to_json(self):
+        info_obj = Info.query.filter_by(id=self.after).first()
+
+        return {
+            'number': info_obj.number,
+            'name': info_obj.name,
+            'type': info_obj.type.name
+        }
+
+
 class Info(db.Model):
     __table_args__ = {'mysql_charset': 'utf8mb4'}
 
@@ -17,9 +34,12 @@ class Info(db.Model):
     type = db.Column(db.Enum(PokemonType), default=PokemonType.Grass)
 
     def to_json(self):
+        evolution_objs = Evolution.query.filter_by(id=self.id).all()
+
         return {
             'id': self.id,
             'number': self.number,
             'name': self.name,
-            'type': self.type.name
+            'type': self.type.name,
+            'evolutions': [obj.to_json() for obj in evolution_objs]
         }
