b9be5fd207fee87ff4334e5e3c731b6b4ca73584
davidchang <davidchang@ovotv.com>
2021-06-07 03:51:14 +0000

    add type model and update parameter "type -> types"

--- basic/models.py
+++ basic/models.py
@@ -8,6 +8,14 @@ class PokemonType(enum.Enum):
     Fire = 3
 
 
+class Type(db.Model):
+    __table_args__ = {'mysql_charset': 'utf8mb4'}
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    pid = db.Column(db.BigInteger)
+    type = db.Column(db.Enum(PokemonType), default=PokemonType.Grass)
+
+
 class Evolution(db.Model):
     __table_args__ = {'mysql_charset': 'utf8mb4'}
 
@@ -17,11 +25,12 @@ class Evolution(db.Model):
 
     def to_json(self):
         info_obj = Info.query.filter_by(id=self.after).first()
+        type_objs = Type.query.filter_by(pid=info_obj.id).all()
 
         return {
             'number': info_obj.number,
             'name': info_obj.name,
-            'type': info_obj.type.name
+            'type': [type_obj.type.name for type_obj in type_objs]
         }
 
 
@@ -31,15 +40,15 @@ class Info(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     number = db.Column(db.String(10))
     name = db.Column(db.String(100))
-    type = db.Column(db.Enum(PokemonType), default=PokemonType.Grass)
 
     def to_json(self):
         evolution_objs = Evolution.query.filter_by(id=self.id).all()
+        type_objs = Type.query.filter_by(pid=self.id).all()
 
         return {
             'id': self.id,
             'number': self.number,
             'name': self.name,
-            'type': self.type.name,
+            'type': [type_obj.type.name for type_obj in type_objs],
             'evolutions': [obj.to_json() for obj in evolution_objs]
         }
